b7116f0f361f31d6e7efff96c69ff6c96eb255ea
Katie Martinez <martinek@newschool.edu>
2018-09-18 11:16:45 -0400

    update site config project and org

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'The New School', // Title for your website.
   tagline: 'Archives & Special Collections Documentation',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://katiemartinez.github.io', // Your website URL
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
